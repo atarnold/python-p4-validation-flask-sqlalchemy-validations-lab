35feaf40048a318cfcdc3cd24f1f23cc463e04f9
arnold <arnoldkmaina@gmail.com>
2024-07-05 12:13:22 +0300

    python-p4-validation-flask-sqlalchemy-validations-lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,21 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("name field required.")
+        author = db.session.query(Author.id).filter_by(name=name).first()
+        if author is not None:
+            raise ValueError("Name must be unique")
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError("Phone number must be 10 digits.")
+        return phone_number
+
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,8 +42,33 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators 
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Title field required.")
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any (bait in title for bait in clickbait):
+            raise ValueError("No clickbait in title.")
+        return title
+        
+    @validates('category')
+    def validate_Category(self, key, category):
+        if category != "Fiction" and category != "Non-Fiction":
+            raise ValueError("category must be either fiction or non-fiction.")
+        return category
+
+    @validates('content', 'summary')
+    def validate_content(self, key, string):
+        if key == 'content':
+            if len(string) < 250:
+                raise ValueError("Post content must be atleast 250 characters long.") 
+        if key == 'summary':
+            if len(string) > 250:
+                raise ValueError("Post summary must not e greater than 250 characters")
+            return string
 
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+    
